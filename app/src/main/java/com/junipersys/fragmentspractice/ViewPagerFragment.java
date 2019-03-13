54b94a08251659b576f238bcd804d085f7232b5b
Austin.adams <austin.adams@junipersys.com>
2019-03-13 12:40:33 -0600

    added view pager

--- app/src/main/java/com/junipersys/fragmentspractice/ViewPagerFragment.java
+++ app/src/main/java/com/junipersys/fragmentspractice/ViewPagerFragment.java
@@ -3,7 +3,10 @@ package com.junipersys.fragmentspractice;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +18,40 @@ public class ViewPagerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
         int index = getArguments().getInt(KEY_RECIPE_INDEX); //getting bundle arguments
+        View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        final IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(KEY_RECIPE_INDEX, index);
+        ingredientsFragment.setArguments(bundle);
+        final DirectionsFragment directionsFragment = new DirectionsFragment();
 
         //setting the action bar title
         getActivity().setTitle(Recipes.names[index]);
+
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return i == 0 ? ingredientsFragment : directionsFragment;
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return position == 0 ? "Ingredients" : "Directions";
+            }
+
+            @Override
+            public int getCount() { //how many fragments does this track
+                return 2;
+            }
+        });
+
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
         return view;
     }
 
